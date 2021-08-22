c1aa09619928e39eb829b6240634a06a75b27197
Kamil <>
2021-08-22 11:34:15 +0200

    empty cart bug fixed

--- src/Pages/FinaliseOrder/MealSummary.js
+++ src/Pages/FinaliseOrder/MealSummary.js
@@ -1,4 +1,5 @@
 import MealPrice from "./MealPrice";
+import "./MealSummary.css";
 import {useState} from "react";
 
 function MealSummary(props) {
@@ -6,8 +7,10 @@ function MealSummary(props) {
         <li className="meals-list" key={props.meal.id}>
             <div className="meal-name">
                 <p>{props.meal.name}</p>
-                <p>x{props.count}</p>
-                <MealPrice meal={props.meal} mealsCount={props.count}/>
+                <div className="meal-price-display">
+                    <p>x{props.count}</p>
+                    <MealPrice meal={props.meal} mealsCount={props.count}/>
+                </div>
             </div>
         </li>
     )
--- src/Pages/FinaliseOrder/OrderSummary.js
+++ src/Pages/FinaliseOrder/OrderSummary.js
@@ -17,12 +17,22 @@ function OrderSummary(props) {
     return(
         <div className="board-content-container">
             <h1 className="summery-title"><u>Podsumowanie:</u></h1>
-            <h2 id="order-id">Zamówienie nr <strong>{props.order[0].orderId}</strong></h2>
+            {/*{console.log("summery", props.order.length, props.order > 0)}*/}
             {props.error && <p>{props.error}</p>}
-            {!props.error && <OrderedMeals meals={props.order} />}
-            {!props.error && <OrderSum meals={props.order} />}
-            <PayCheckbox onCheck={checkHandler} />
-            <SummaryBtns />
+            {(!props.error && props.order.length > 0) ?
+                (<div>
+                    <h2 id="order-id">Zamówienie nr <strong>{props.order[0].orderId}</strong></h2>
+                    <OrderedMeals meals={props.order} />
+                    <OrderSum meals={props.order} />
+                    <PayCheckbox onCheck={checkHandler} />
+                    <SummaryBtns />
+                </div>) :
+                <p>Koszyk jest pusty</p>
+            }
+            {/*{props.order > 0 && <h2 id="order-id">Zamówienie nr <strong>{props.order[0].orderId}</strong></h2>}*/}
+            {/*{!props.error && props.order > 0  && <OrderedMeals meals={props.order} />}*/}
+            {/*{!props.error && props.order > 0 && <OrderSum meals={props.order} />}*/}
+            {/*{!props.error && props.order <= 0 && <p>Koszyk jest pusty</p>}*/}
         </div>
     )
 }
