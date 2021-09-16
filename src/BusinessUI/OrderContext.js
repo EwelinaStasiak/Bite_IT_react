a0d012c300ac035c2b1bfbd59a563410a34eb7a5
Kamil <>
2021-09-16 16:53:21 +0200

    orderReducer modified for orderContext

--- src/BusinessUI/OrderContext.js
+++ src/BusinessUI/OrderContext.js
@@ -4,14 +4,22 @@ import {cartActions} from "../Utility/_cartActions";
 const Context = React.createContext();
 Context.displayName = 'Order';
 
-export const initOrder = {orderId: "", orderLines: []};
+export const initOrder = {orderId: "", cart: []};
 export const useOrder = () => React.useContext(Context);
 
 export const orderReducer = (prevState, action) => {
         switch (action.type){
             case cartActions.add:
-                prevState.orderLines.push(action.cart);
-                return {...prevState, orderLines: prevState.orderLines}
+                prevState.cart.push(action.meal);
+                // console.log("new cart: ", prevState.cart);
+                return {...prevState, cart: prevState.cart}
+            case cartActions.remove:
+                const otherMeals = prevState.cart.filter(m => m.id !== action.meal.id);
+                const modifiedMeals = prevState.cart.filter(m => m.id === action.meal.id)
+                modifiedMeals.pop();
+                // console.log("new cart: ", newCart);
+                // return {...prevState, cart: newCart}
+                return {...prevState, cart: [...otherMeals, ...modifiedMeals]}
         }
 }
 
