7f58061c21289d8ff2bb1a9d0079f614ebeab3f0
Kamil <>
2021-09-16 16:56:04 +0200

    MealDetails component refactored - unused functions and variables removed

--- src/BusinessUI/UserInterface/Subpages/DisplayMenu/MealDetails.js
+++ src/BusinessUI/UserInterface/Subpages/DisplayMenu/MealDetails.js
@@ -1,7 +1,5 @@
 import MealAdditionalInfo from "./MealAdditionalInfo";
 import {useState} from "react";
-import {useOrder} from "../../../OrderContext";
-import {cartActions} from "../../../../Utility/_cartActions";
 import EditOrderRow from "./EditOrderRow";
 
 const MealDetails = (props) => {
@@ -13,32 +11,16 @@ const MealDetails = (props) => {
         setShownStatus(!isShown);
     }
     
-    const order = useOrder().order;
-    const dispatch = useOrder().dispatch;
-    
-    const clickHandler = () => {
-        dispatch({
-            type: cartActions.add,
-            cart: {mealId: 1}
-        })
-    }
-    
-    // console.log("cart: ", order.orderLines);
- 
-    
     return (
         <div className="table-meal-details">
             <tr key={meal.id} onClick={toggleShown}>
                 <td className="table-meal-name">{meal.name}</td>
                 <td className="table-meal-price" rowSpan="4">{meal.price} <span>PLN</span></td>
-                <td className="table-add-btn" rowSpan="4"><button onClick={clickHandler}>Dodaj</button></td>
             </tr>
             <tr>
                 <EditOrderRow
                     meal={meal}
                     meals={meals}
-                    cart={props.cart}
-                    setCart={props.setCart}
                 />
             </tr>
             {isShown &&
